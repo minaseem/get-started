c86d9f4dda40b88077ad32cb769c0f5150aa1c6b
Imamudin Naseem <naseem1992@gmail.com>
2017-10-12 12:19:08 +0530

    proxy server

---
+++ proxy.js
@@ -0,0 +1,16 @@
+/**
+ * Created by imamudinnaseem on 5/22/17.
+ */
+
+var express = require('express');
+var app = express();
+var proxy = require('express-http-proxy');
+
+
+app.use('/polymer', proxy('https://shop.polymer-project.org'))
+app.get('/api/data', (req, resp) => {
+    console.log("API HIT");
+    resp.send("Hello world")
+});
+app.listen(4001)
+console.log('listening to port ' + 4001)
